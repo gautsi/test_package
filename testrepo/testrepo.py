63c4e8e51ee4f79d768ded4616180aa97bd83353
Gautam <gautam.sisodia@gmail.com>
2015-07-26 17:07:34 -0400

    modified tox.ini to include pytest doctesting

--- testrepo/testrepo.py
+++ testrepo/testrepo.py
@@ -2,11 +2,13 @@
 def test_func():
     """
     A test function for doctests and unitests with tox.
-    
     For example,
-    
+
     >>> test_func()
     'hello'
-    
+
+    >>> 1 + 2
+    3
+
     """
     return "hello"
